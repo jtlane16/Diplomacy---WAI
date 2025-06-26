e85587a6836a84ac0440534a416b50a523083043
Jackson Lane <Jackson Lane@GamingPC>
2025-06-26 01:31:44 -0400

    Feast Updates

--- Bannerlord.Diplomacy/TodayWeFeast/Feasts.cs
+++ Bannerlord.Diplomacy/TodayWeFeast/Feasts.cs
@@ -13,40 +13,38 @@ using TaleWorlds.SaveSystem;
 
 namespace TodayWeFeast
 {
-	public class FeastObject
+    public class FeastObject
     {
-		public FeastObject(Settlement feastSettlementPar, Kingdom kingdomPar, List<Hero> lordsInFeastPar, Hero hostOfFeast, float foodContribution)
+        public FeastObject(Settlement feastSettlementPar, Kingdom kingdomPar, List<Hero> lordsInFeastPar, Hero hostOfFeast, float foodContribution)
         {
-			this.feastSettlement = feastSettlementPar;
-			this.kingdom = kingdomPar;
-			this.lordsInFeast = lordsInFeastPar;
-			this.hostOfFeast = hostOfFeast;
-			this.sendAllLordsToFeast();
-			this.createTournamentIfApplicable(feastSettlementPar);
-			if (foodContribution != -1)
+            this.feastSettlement = feastSettlementPar;
+            this.kingdom = kingdomPar;
+            this.lordsInFeast = lordsInFeastPar;
+            this.hostOfFeast = hostOfFeast;
+            this.sendAllLordsToFeast();
+            this.createTournamentIfApplicable(feastSettlementPar);
+            if (foodContribution != -1)
             {
-				this.makeAILordContributeToTheFeast(hostOfFeast, foodContribution);
-			}
-		}
+                this.makeAILordContributeToTheFeast(hostOfFeast, foodContribution);
+            }
+        }
 
-		public void createTournamentIfApplicable(Settlement feastSettlement)
+        public void createTournamentIfApplicable(Settlement feastSettlement)
         {
-			if (feastSettlement.IsTown)
-			{
-				Town town = feastSettlement.Town;
-				ITournamentManager tournamentManager = Campaign.Current.TournamentManager;
-				TournamentGame tournamentGame = tournamentManager.GetTournamentGame(town);
-				if (tournamentGame == null)
-				{
-					tournamentManager.AddTournament(Campaign.Current.Models.TournamentModel.CreateTournament(town));
-				}
-			}
-		}
+            if (feastSettlement.IsTown)
+            {
+                Town town = feastSettlement.Town;
+                ITournamentManager tournamentManager = Campaign.Current.TournamentManager;
+                TournamentGame tournamentGame = tournamentManager.GetTournamentGame(town);
+                if (tournamentGame == null)
+                {
+                    tournamentManager.AddTournament(Campaign.Current.Models.TournamentModel.CreateTournament(town));
+                }
+            }
+        }
 
-        // In the FeastObject class, modify the sendAllLordsToFeast method
         public void sendAllLordsToFeast()
         {
-            // Existing notifications for when the player is the host or is invited
             if (this.hostOfFeast == Hero.MainHero)
             {
                 InformationManager.ShowInquiry(new InquiryData("Today, We Feast!", $"You have decided to host a feast, where the lords of the realm will gather at {this.feastSettlement.Name} to celebrate with you!", true, false, "OK", null, null, null), true);
@@ -56,8 +54,6 @@ namespace TodayWeFeast
                 InformationManager.ShowInquiry(new InquiryData("Feast Invite", $"You have received an invitation to a feast hosted by {this.hostOfFeast.Name} at his home, {this.feastSettlement.Name}", true, false, "OK", null, null, null), true);
             }
 
-            // --- ADD THIS NEW LOGIC ---
-            // If the feast is in the player's kingdom, but not hosted by the player, show a map notification.
             if (this.hostOfFeast != Hero.MainHero && this.kingdom == Hero.MainHero.MapFaction)
             {
                 TextObject message = new TextObject("{=feast_notification}A feast has begun at {SETTLEMENT_NAME}, hosted by {HOST_NAME}!");
@@ -65,19 +61,15 @@ namespace TodayWeFeast
                 message.SetTextVariable("HOST_NAME", this.hostOfFeast.Name);
                 MBInformationManager.AddQuickInformation(message);
             }
-            // --- END NEW LOGIC ---
         }
 
         public void endFeast()
-		{
-            // --- Start of new reward logic ---
+        {
             InformationManager.DisplayMessage(new InformationMessage($"The feast at {this.feastSettlement.Name} has ended.", Colors.Green));
 
-            // Grant rewards to the host
             this.hostOfFeast.Clan.AddRenown(50, true);
             this.hostOfFeast.AddSkillXp(DefaultSkills.Steward, 1000);
 
-            // Grant relation to attendees
             foreach (Hero hero in this.lordsInFeast)
             {
                 if (hero.PartyBelongedTo != null && hero != Hero.MainHero)
@@ -85,116 +77,118 @@ namespace TodayWeFeast
                     hero.PartyBelongedTo.Ai.SetDoNotMakeNewDecisions(false);
                 }
 
-                // Grant a final relation boost
                 if (hero != this.hostOfFeast)
                 {
                     ChangeRelationAction.ApplyRelationChangeBetweenHeroes(this.hostOfFeast, hero, 5, true);
                 }
             }
-            // --- End of new reward logic ---
 
-            //End the tournament at the feast if there is one
             if (this.feastSettlement.IsTown)
             {
                 Town town = this.feastSettlement.Town;
-				ITournamentManager tournamentManager = Campaign.Current.TournamentManager;
-				TournamentGame tournamentGame = tournamentManager.GetTournamentGame(town);
-				if (tournamentGame != null)
-				{
-					tournamentManager.ResolveTournament(tournamentGame, town);
-				}
-			}
-            FeastBehavior.Instance.Feasts.Remove(this);
-            FeastBehavior.Instance.timeSinceLastFeast.Add(this.kingdom, CampaignTime.Now.ToDays);
-            InformationManager.DisplayMessage(new InformationMessage("The feast at " + this.feastSettlement.Name.ToString() + " has ended."));
-		}
-
-		public void dailyFeastTick()
-        {
-			if (this.amountOfFood <= 0 || (CampaignTime.Now.ToDays > this.endFeastDay.ToDays))
-            {
-				this.endFeast();
-				return;
+                ITournamentManager tournamentManager = Campaign.Current.TournamentManager;
+                TournamentGame tournamentGame = tournamentManager.GetTournamentGame(town);
+                if (tournamentGame != null)
+                {
+                    tournamentManager.ResolveTournament(tournamentGame, town);
+                }
             }
-			else
+            FeastBehavior.Instance.Feasts.Remove(this);
+            if (!FeastBehavior.Instance.timeSinceLastFeast.ContainsKey(this.kingdom))
             {
-				foreach (Hero hero in this.lordsInFeast)
-				{
-					if (this.amountOfFood <= 0 && this.hostOfFeast == Hero.MainHero && CampaignTime.Now.ToDays > playerTimeBeforeEnd.ToDays)
-                    {
-						this.endFeast();
-						return;
-					}
-					else if (this.amountOfFood <= 0 && this.hostOfFeast != Hero.MainHero)
-                    {
-						this.endFeast();
-						return;
-					}
-					else if (hero.CurrentSettlement == this.feastSettlement)
-                    {
-						try
-                        {
-							this.feastRoster.AddToCounts(this.feastRoster[1].EquipmentElement, -1);
-							this.amountOfFood--;
-						} catch {
-							this.feastRoster.AddToCounts(this.feastRoster[0].EquipmentElement, -1);
-							this.amountOfFood--;
-						}
-
-						
-					}
-				}
-			}
-
+                FeastBehavior.Instance.timeSinceLastFeast.Add(this.kingdom, CampaignTime.Now.ToDays);
+            }
         }
 
-		public void makeAILordContributeToTheFeast(Hero hero, float foodToContribute)
+        public void dailyFeastTick()
         {
-			//Copy the original roster
-			float count = 0f;
-			for (int i = 0; i < hero.PartyBelongedTo.ItemRoster.Count; i++)
+            if (this.hostOfFeast != Hero.MainHero)
             {
-				if (count > foodToContribute)
-				{
-					break;
-				} else if (!hero.PartyBelongedTo.ItemRoster[i].EquipmentElement.Item.IsFood)
+                foreach (Hero guest in this.lordsInFeast)
                 {
-					continue;
-                } else
-                {
-					var tempItemCount = 0;
-					while (tempItemCount < hero.PartyBelongedTo.ItemRoster[i].Amount && count < foodToContribute)
+                    if (guest != this.hostOfFeast && guest != Hero.MainHero)
                     {
-						tempItemCount++;
-						this.feastRoster.AddToCounts(hero.PartyBelongedTo.ItemRoster[i].EquipmentElement, 1);
-						hero.PartyBelongedTo.ItemRoster.AddToCounts(hero.PartyBelongedTo.ItemRoster[i].EquipmentElement, -1);
-						count++;
+                        ChangeRelationAction.ApplyRelationChangeBetweenHeroes(guest, this.hostOfFeast, 1, false);
                     }
-
-				}
-
+                }
             }
-			this.amountOfFood = count;
+
+            if (this.amountOfFood <= 0 || (CampaignTime.Now.ToDays > this.endFeastDay.ToDays))
+            {
+                this.endFeast();
+                return;
+            }
+            else
+            {
+                foreach (Hero hero in this.lordsInFeast)
+                {
+                    if (this.amountOfFood <= 0 && this.hostOfFeast == Hero.MainHero && CampaignTime.Now.ToDays > playerTimeBeforeEnd.ToDays)
+                    {
+                        this.endFeast();
+                        return;
+                    }
+                    else if (this.amountOfFood <= 0 && this.hostOfFeast != Hero.MainHero)
+                    {
+                        this.endFeast();
+                        return;
+                    }
+                    else if (hero.CurrentSettlement == this.feastSettlement)
+                    {
+                        try
+                        {
+                            this.feastRoster.AddToCounts(this.feastRoster[1].EquipmentElement, -1);
+                            this.amountOfFood--;
+                        }
+                        catch
+                        {
+                            this.feastRoster.AddToCounts(this.feastRoster[0].EquipmentElement, -1);
+                            this.amountOfFood--;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void makeAILordContributeToTheFeast(Hero hero, float foodToContribute)
+        {
+            if (hero.PartyBelongedTo == null) return;
+            if (hero.PartyBelongedTo.ItemRoster == null) return;
+
+            float count = 0f;
+            for (int i = 0; i < hero.PartyBelongedTo.ItemRoster.Count; i++)
+            {
+                if (count > foodToContribute) break;
+                else if (!hero.PartyBelongedTo.ItemRoster[i].EquipmentElement.Item.IsFood) continue;
+                else
+                {
+                    var tempItemCount = 0;
+                    while (tempItemCount < hero.PartyBelongedTo.ItemRoster[i].Amount && count < foodToContribute)
+                    {
+                        tempItemCount++;
+                        this.feastRoster.AddToCounts(hero.PartyBelongedTo.ItemRoster[i].EquipmentElement, 1);
+                        hero.PartyBelongedTo.ItemRoster.AddToCounts(hero.PartyBelongedTo.ItemRoster[i].EquipmentElement, -1);
+                        count++;
+                    }
+                }
+            }
+            this.amountOfFood = count;
         }
 
         [SaveableField(11)]
-		public Settlement feastSettlement;
-		[SaveableField(12)]
-		public CampaignTime endFeastDay = CampaignTime.DaysFromNow(20f);
-		[SaveableField(18)]
-		public CampaignTime playerTimeBeforeEnd = CampaignTime.DaysFromNow(2f);
-		[SaveableField(13)]
-		public Kingdom kingdom;
-		[SaveableField(14)]
-		public List<Hero> lordsInFeast;
-		[SaveableField(15)]
-		public Hero hostOfFeast;
-		[SaveableField(16)]
-		public float amountOfFood;
-		[SaveableField(17)]
-		public ItemRoster feastRoster = new ItemRoster();
-
-	}
-
+        public Settlement feastSettlement;
+        [SaveableField(12)]
+        public CampaignTime endFeastDay = CampaignTime.DaysFromNow(20f);
+        [SaveableField(18)]
+        public CampaignTime playerTimeBeforeEnd = CampaignTime.DaysFromNow(2f);
+        [SaveableField(13)]
+        public Kingdom kingdom;
+        [SaveableField(14)]
+        public List<Hero> lordsInFeast;
+        [SaveableField(15)]
+        public Hero hostOfFeast;
+        [SaveableField(16)]
+        public float amountOfFood;
+        [SaveableField(17)]
+        public ItemRoster feastRoster = new ItemRoster();
+    }
 }
-
